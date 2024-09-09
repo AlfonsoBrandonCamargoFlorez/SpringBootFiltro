e96b5123e59b4e3aa5f2ddd7bd359ce77719389a
AlfonsoBrandonCamargoFlorez <b.c.florez@hotmail.com>
2024-09-09 18:31:09 -0500

    Correccion Git

--- src/main/java/com/charlies/beach/infrastructure/repository/repository/VentasRepository.java
+++
@@ -1,9 +0,0 @@
-package com.charlies.beach.infrastructure.repository.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.charlies.beach.domain.entities.Ventas;
-
-public interface VentasRepository extends JpaRepository <Ventas, Long> {
-
-}
